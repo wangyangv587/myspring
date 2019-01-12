ff08787c10730c2cbf8156fe61ee2d2252b3d536
yangwangv5876 <yangwangv5876@163.com>
2019-01-12 18:18:47 +0800

    添加aspectj实现的声明式事务控制（注解方式）

--- src/main/java/com/kaikeba/service/impl/AccountServiceImpl.java
+++ src/main/java/com/kaikeba/service/impl/AccountServiceImpl.java
@@ -4,7 +4,12 @@ import com.kaikeba.dao.AccountDao;
 import com.kaikeba.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
+//@Transactional表示该类所有方法都被事务进行管理了
+@Transactional( propagation = Propagation.REQUIRED,isolation = Isolation.DEFAULT)
 @Service
 public class AccountServiceImpl implements AccountService {
 
